d19b9f3b17b21e54f002f5f2981fcd6aca40bdf4
Dimeji Abidoye <dimejy2@gmail.com>
2015-09-17 13:07:11 -0400

    final updates

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# kfcap
-A small REST app written in node to find kfc's in Nairobi and keep track of their inventories. 
+## KFC API 
+
+A small REST API written in Node.js with express to serve as the MVP backend for a restaurant 
+location and menu storage app. 
+
+##/location
+    *Post: post body( query )
+        posting to this endpoint will update the locations present in the DB. Adding in the query   
+        to the post body will determine the search string used to update the databse. Otherwise it 
+        defaults to kfc.
+    *GET : query ( lat, lng, rad)
+        this returns all the locations within a search radius rad (in km) in increasing order of their  
+        distance from the geopoint (lat, lng). If any of these is omitted then it defaults to the geopoint
+        representing the Nairobi city center. 
+
+
+##/menu
+    *Post: post body( item, price)
+        posting to this endpoint will add a new item with price to the database. 
+
+    *Get: query(price)
+        this returns all the items in the menu database. including a non-null price will return 
+        the items in addition to their prices.
+        
+        
--- routes/index.js
+++ routes/index.js
@@ -8,108 +8,105 @@ var proximity = require('geo-proximity').initialize(client)
 var url = require('url') ;
 
 router.get('/', function(req, res, next) {
-	res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
 
-router.get('/update', function(req, res){
+router.route('/location')
+.post(function(req, res){
+    var queryString ="KFC, Nairobi, restaurant";   
+    if (req.body.query) queryString = req.body.query;  
+    parameters = {
+        query: queryString 
+    };
+    googlePlaces.textSearch(parameters, function (err, response) {
+        var locations = []; 
+        if(err){ res.send(err); }
 
-	parameters = {
-		query:"KFC, Nairobi, kenya, restaurant" 
-	};
-	googlePlaces.textSearch(parameters, function (err, response) {
-		var locations = []; 
-		if(response === null){
-			console.log(err); 
-			return; 
-		}
+        var results  = response.results; 
+        for(i = 0; i < results.length; i++){
+            console.log(i); 
+            temp = []; 
+            temp[0] = results[i].geometry.location.lat; 
+            temp[1] =results[i].geometry.location.lng;  	
+            temp[2] = results[i].formatted_address; 
+            locations[i] = temp; 
+        }	
+        proximity.addLocations(locations, function(err, reply){
+            if(err) console.error(err); 
+            else console.log('added locations:', reply); 
+        }); 			
+        res.send({"Success" :true, "message" : "updated locations"});
+        res.end(); 
+    });
+})
+.get(function(req, res){
+    var url_parts = url.parse(req.url, true);
+    var query = url_parts.query; 
+    var lat = query.lat;
+    var lng = query.lng; 
+    var rad = query.rad; 
+    if(!lat || !lng || !rad ){		
 
+        closest(res, -1.3048035,36.8473969, 10000); 
+    }	else{
+        console.log(lat);
 
-		var results  = response.results; 
-		for(i = 0; i < results.length; i++){
-			console.log(i); 
-			temp = []; 
-			temp[0] = results[i].geometry.location.lat; 
-			temp[1] =results[i].geometry.location.lng;  	
-			temp[2] = results[i].formatted_address; 
-			locations[i] = temp; 
-		}	
-		proximity.addLocations(locations, function(err, reply){
-			if(err) console.error(err); 
-			else console.log('added locations:', reply); 
-		}); 			
-		res.send(locations);
-		res.end(); 
-	});
-}); 
-
-
-
-router.get('/closest', function(req, res){
-	var url_parts = url.parse(req.url, true);
-	var query = url_parts.query; 
-	//console.log(query); 
-	var lat = query.lat;
-	var lng = query.lng; 
-	var rad = query.rad; 
-	if(lat === null || lng === null || rad === null ){		
-
-		closest(res, -1.3048035,36.8473969, 10000); 
-	}	else{
-		console.log(lat);
-
-		closest(res, lat, lng, rad ); 
-	}
+        closest(res, lat, lng, rad ); 
+    }
 
 
 }); 
 
 var closest = function(res, lat, lng, rad){
-	proximity.nearby(lat, lng, rad, function(err, locations){
-		if(err){ console.error(err); 
-			console.log(err); 
-		}else if(locations.length !== 0 ){
-			res.send(locations);
-			res.end();
-		}
-		else{
-			res.send("your search radius does not include any KFC's"); 		
-		}		
-	}); }
+    proximity.nearby(lat, lng, rad, function(err, locations){
+        if(err){ console.error(err); 
+            console.log(err); 
+        }else if(locations.length !== 0 ){
+            res.send(locations);
+            res.end();
+        }
+        else{
+            res.send("your search radius does not include any KFC's"); 		
+        }		
+    }); 
+
+}
 
 
 
-router.get('/addMenuItem', function(req, res){
-	var url_parts = url.parse(req.url, true); 
-	var query = url_parts.query; 
-	var item = query.item; 
-	var price = query.price; 
-	if(item !== null && price !== null){
-		client.sadd("menuset", item); 
-		res.send(client.hset("menu",item,price, redis.print));
-		res.end(); 
-	}
-	else{
-		res.send("please include a valid item and price ")
-			res.end();
-	} 
-}); 
+router.route('/menu')
+.post( function(req, res){
 
-router.get('/allMenuItems', function(req, res){
-	var menuset = client.smembers('menuset', function(err, cont){
-		console.log( cont);
-		res.send(cont);
-		res.end(); 
-	});
-}); 
+    var item = req.body.item; 
+    var price = req.body.price; 
+    if(item && price){
+        //the next three lines are really bad code but I was in a hurry ¯\_(ツ)_/¯... there are no
+        //guarantees that client.sadd and client.hset will suceed but success will be returned
+        //in any case... to whoever forks this, you need to use async.js parallel to fix this. Good luck. 
 
+        client.sadd("menuset", item); 
+        client.hset("menu",item,price, redis.print);  
+        res.send({"Success" : "true" , "message" : "Successfully updated menu"});
+    }
+    else{
+        res.send({"Success" : false , "message" :"please include a valid item and price"}); 
+    }
 
-router.get('/allMenuItemsPrice', function(req, res ){
-	var menuWithPrice = client.hgetall('menu', function(err, cont){
-		console.log(cont); 
-		res.send(cont);
-		res.end(); 	
-	}); 
+}).get(function(req, res){
+    //what follows should really be using some form of streaming... 
+    //bad things can happen if the sets get too large
+    //it should also allow more complex queries. I believe in you. 
+    if(req.query.price) {
+        var menuWithPrice = client.hgetall('menu', function(err, cont){
+            res.send(cont);
+        }); 
 
+    }
+    else{ 
+        var menuset = client.smembers('menuset', function(err, cont){
+            res.send(cont);
+        });
+    }
 }); 
 
 module.exports = router;
